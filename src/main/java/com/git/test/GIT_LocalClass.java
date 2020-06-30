8ef70d260bf122a0a754c538dacbea44cae47d38
Durga Sireesha Mantena <DMantena@logisticsdev.intra>
2020-07-01 00:19:11 +0530

    adding new by local

---
+++ src/main/java/com/git/test/GIT_LocalClass.java
@@ -0,0 +1,12 @@
+package com.git.test;
+
+public class GIT_LocalClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		 System.out.println(" im just local class");
+	}
+
+}
